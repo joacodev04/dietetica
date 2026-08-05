03ba34b777199e0e05ccc0d5f032e538688dbce5
Joaquin <joaquin04murillo@gmail.com>
2026-08-05 01:01:48 -0300

    Nuevo Producto

--- sistema/src/pages/Inventario.jsx
+++ sistema/src/pages/Inventario.jsx
@@ -5,7 +5,6 @@ import InfoProds from '../components/InfoProds/InfoProds'
 
 const Inventario = () => {
   return (
-
     <div className="inventario-container">
       <div className="header-inventario">
         <h1 className="titulo-izq">Sistema de gestión de Stock</h1>
--- sistema/src/pages/NuevoProducto.jsx
+++ sistema/src/pages/NuevoProducto.jsx
@@ -4,6 +4,32 @@ import './NuevoProducto.css'
 const NuevoProducto = () => {
   return (
     <>
+      <div className="inventario-container">
+        <div className="header-inventario">
+          <h1 className="titulo-izq">Sistema de gestión de Stock</h1>
+        <div className="search-box">
+          <input type="text" placeholder="Buscar producto..." />
+          <button>Buscar</button>
+        </div>
+        </div>
+        <section className='container-prod'>
+          <div>
+            <h1>Registrar Nuevo Producto</h1>
+            <p>Complete la informacion detallada para añadir un nuevo producto</p>
+          </div>
+          <div className='container-info-prods'>
+            <h3>Complete los siguientes campos</h3>
+              <input type="name" placeholder="Nombre del producto" />
+              <input type="id" placeholder="Codigo del producto" />
+              <select>
+                <option value="">Seleccione una categoría</option>
+                <option value="proteinas">Proteínas</option>
+                <option value="creatinas">Creatinas</option>
+                <option value="vitaminas">Vitaminas</option>
+              </select>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
